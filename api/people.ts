4e156abf534a0f718a198af897f2ac8179983fa0
ameouchi <56494803+ameouchi@users.noreply.github.com>
2025-08-22 10:02:38 -0600

    fix geo

--- api/people.ts
+++
@@ -1,7 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getPeople } from './_store';
-
-export default async function handler(_req: VercelRequest, res: VercelResponse) {
-  res.setHeader('cache-control', 'no-store');
-  res.status(200).json({ people: getPeople() });
-}
